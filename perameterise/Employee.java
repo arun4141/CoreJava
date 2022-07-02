4bb4a3d194fa034619ba7d415556b21fba130e1d
arun4141 <arunmandloi9775@gmail.com>
2022-07-02 13:34:46 +0530

    message

---
+++ perameterise/Employee.java
@@ -0,0 +1,20 @@
+package com.rays.perameterise;
+
+public class Employee extends person {
+	
+	private String designation;
+	
+	public Employee () {
+		
+		System.out.println("defoult employee");
+	}
+               public Employee (String fn,String ln,String des) {
+            	   super(fn,ln);
+            	   designation = des;
+            	   System.out.println("3 perametar Employee" + designation);
+               }
+              public static void main(String[] args) {
+				Employee e = new Employee("vijay", "jain", "developar"); 
+			}
+
+}
